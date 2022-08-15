f3b8a007dd932108378cc4973776b24830494bd9
Gabriel <gabriel.scardozo7@gmail.com>
2022-08-15 14:28:12 -0300

    Versao-final

--- script.js
+++ script.js
@@ -4,24 +4,31 @@ let bebidaselecionado=0;
 let sobremesaselecionado=0;
 let pedidofechado=0;
 
+let prato;
+let precoprato;
+let bebida;
+let precobebida;
+let sobremesa;
+let precosobremesa;
+let soma;
+
 
 function SelecionarPrato(PratoN){
     pratoselecionado = document.querySelector('.Pratos .selecionado');
-
     if(pratoselecionado !== null) {pratoselecionado.classList.remove('selecionado');
 }
-
     PratoN.classList.add('selecionado');
-
     if(pratoselecionado !== 0 && bebidaselecionado !== 0 && sobremesaselecionado !== 0){
         pedidofechado = document.querySelector('.selecione');
         pedidofechado.classList.add('pedidocompleto');
         pedidofechado.innerHTML = "Fechar pedido";
     }
-
-    let checkhidden = document.querySelector()
-
-
+    let checkhidden = document.querySelector('.Pratos .aparece');
+    if(checkhidden !== null){
+        checkhidden.classList.remove('aparece');
+    }
+    let checkblock = document.querySelector('.Pratos .selecionado .esconde');
+    checkblock.classList.add('aparece');
 }
 
 function SelecionarBebida(BebidaN){
@@ -33,6 +40,12 @@ function SelecionarBebida(BebidaN){
         pedidofechado.classList.add('pedidocompleto');
         pedidofechado.innerHTML = "Fechar pedido";
     }
+    let checkhidden2 = document.querySelector('.Bebidas .aparece');
+    if(checkhidden2 !== null){
+        checkhidden2.classList.remove('aparece');
+    }
+    let checkblock2 = document.querySelector('.Bebidas .selecionado .esconde');
+    checkblock2.classList.add('aparece');
 }
 
 function SelecionarSobremesa(SobremesaN){
@@ -44,6 +57,12 @@ function SelecionarSobremesa(SobremesaN){
         pedidofechado.classList.add('pedidocompleto');
         pedidofechado.innerHTML = "Fechar pedido";
     }
+    let checkhidden3 = document.querySelector('.Sobremesas .aparece');
+    if(checkhidden3 !== null){
+        checkhidden3.classList.remove('aparece');
+    }
+    let checkblock3 = document.querySelector('.Sobremesas .selecionado .esconde');
+    checkblock3.classList.add('aparece');
 }
 
 function fecharpedido(){
@@ -55,14 +74,14 @@ function fecharpedido(){
 
 
 
-    let precoprato = Number(document.querySelector('.Pratos .selecionado .descricao .preco').innerHTML.replace(',','.'));
-    let precobebida = Number(document.querySelector('.Bebidas .selecionado .descricao .preco').innerHTML.replace(',','.'));
-    let precosobremesa = Number(document.querySelector('.Sobremesas .selecionado .descricao .preco').innerHTML.replace(',','.'));
-    let soma = precobebida + precoprato + precosobremesa;
+    precoprato = Number(document.querySelector('.Pratos .selecionado .descricao .preco').innerHTML.replace(',','.'));
+    precobebida = Number(document.querySelector('.Bebidas .selecionado .descricao .preco').innerHTML.replace(',','.'));
+    precosobremesa = Number(document.querySelector('.Sobremesas .selecionado .descricao .preco').innerHTML.replace(',','.'));
+    soma = precobebida + precoprato + precosobremesa;
 
-    let prato = document.querySelector('.Pratos .selecionado .descricao .itemsolicitado').innerHTML;
-    let bebida = document.querySelector('.Bebidas .selecionado .descricao .itemsolicitado').innerHTML;
-    let sobremesa = document.querySelector('.Sobremesas .selecionado .descricao .itemsolicitado').innerHTML;
+    prato = document.querySelector('.Pratos .selecionado .descricao .itemsolicitado').innerHTML;
+    bebida = document.querySelector('.Bebidas .selecionado .descricao .itemsolicitado').innerHTML;
+    sobremesa = document.querySelector('.Sobremesas .selecionado .descricao .itemsolicitado').innerHTML;
     
 
     let pratinho = document.querySelector('.precopratoselecionado');
@@ -87,9 +106,19 @@ function fecharpedido(){
     hidden.classList.remove('esconde');
 }
 
-
     function cancelar (){
         let cancelar = document.querySelector('.teladeenvio');
         cancelar.classList.add('esconde');
+    }
+
+    function podepedir(){
+        let pedido = encodeURIComponent( `Olá, gostaria de fazer o pedido:
+        - Prato: ${prato}
+        - Bebida: ${bebida}
+        - Sobremesa: ${sobremesa}
+        Total: R$ ${soma.toFixed(2)}`);
+
+        let link = document.querySelector('.trocarlink');
+        link.href = (`https://wa.me/5511994703386?text=${pedido}`);
     }
 
